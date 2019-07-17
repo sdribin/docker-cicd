b7b904bf73ff019e89a01a830cd02d17cf79a24d
Semyon Dribin <semyon.dribin@forescout.com>
2019-07-17 11:52:19 +0300

    fix

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,7 +1,7 @@
 job('NodeJS example') {
     scm {
         git('git@github.com:sdribin/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('sdribin)
+            node / gitConfigName('sdribin')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
